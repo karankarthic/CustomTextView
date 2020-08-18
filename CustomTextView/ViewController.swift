8b42d8e5b6e99cd6b50843b92651889c26fa913b
karankarthic.n <karankarthic.n@zohocorp.com>
2020-08-18 16:17:52 +0530

    Initial Commit

---
+++ CustomTextView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CustomTextView
+//
+//  Created by Karan Karthic on 18/08/20.
+//  Copyright © 2020 Karan Karthic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
